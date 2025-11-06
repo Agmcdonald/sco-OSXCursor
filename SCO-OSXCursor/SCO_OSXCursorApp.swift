29a0d1d273822da709aab31cefa75f950eb5b026
Andrew McDonald <andrewmnj@gmail.com>
2025-11-05 21:11:55 -0500

    Initial Commit

---
+++ SCO-OSXCursor/SCO_OSXCursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCO_OSXCursorApp.swift
+//  SCO-OSXCursor
+//
+//  Created by Andrew McDonald on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SCO_OSXCursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
